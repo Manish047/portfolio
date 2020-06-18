ee62cca01bd268d0b3b9e820085eb264c7a6ed89
Manish Sawlani <sawlanim9@gmail.com>
2020-06-18 19:20:16 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -15,5 +15,5 @@ const playAnimation = () => {
         buttonExplore.classList.remove('animate-button');
     }, 2000);
 }
-document.onload = playAnimation;
+window.onload = playAnimation;
 homeLink.addEventListener('click', playAnimation);
