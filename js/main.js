3aadddd9fcdbc3387ef84a5a6975af4ca713f5e0
Manish Sawlani <sawlanim9@gmail.com>
2020-06-18 19:33:25 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var body = document.querySelector('body');
-var loadingscreen = document.getElementById('loadingscreen');
 var homeLink = document.getElementById('homeLink');
 var personName = document.querySelector('.person-name');
 var quote = document.querySelector('.quote');
@@ -7,7 +6,6 @@ var buttonExplore = document.getElementById('explore');
 
 const playAnimation = () => {
     body.style.display = 'block';
-    loadingscreen.style.display = 'none';
     personName.classList.add('animate-name');
     quote.classList.add('animate-quote');
     buttonExplore.classList.add('animate-button');
